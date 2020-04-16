b8f27827f464cbab83a22c7a1858031d388d7254
Karla Agraz <karla.agraz@gmail.com>
2020-04-16 17:09:39 -0500

    Construyendo el componente de certificates y experience

---
+++ src/components/Certificates.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Certicates = () => (
+    <div className="Certicates">
+        <div className="Certicates-container">
+            <div className="Certicates-item">
+                <h3>Desarrollo Web</h3>
+                <p>Este es mi primer certificado</p>
+            </div>
+        </div>
+    </div>
+);
+
+export default Certicates;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Experience = () => (
+    <div className="Experience">
+        <div className="Experience-container">
+            <div className="Experience-item">
+                <h3>Trabajo 1</h3>
+                <p>Este es mi primer trabajo</p>
+            </div>
+        </div>
+    </div>
+);
+
+export default Experience;
