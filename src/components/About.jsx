6260192720637c09a83962a13317de60ef8f599d
Karla Agraz <karla.agraz@gmail.com>
2020-04-16 16:44:38 -0500

    Construyendo el componente ed education y añadiendo sus clases

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const About = () => (
+    <div className="About">
+        <div className="About-container">
+            <div className="About-avatar">
+                <figure>
+                    <img src="" alt=""/>
+                </figure>
+            </div>
+            <div className="About-name">
+                <h2>Karla Agraz</h2>
+            </div>
+            <div className="About-profession">
+                <p>FrontEnd Developer</p>
+            </div>
+            <div className="About-desc">
+                <p>Soy desarrolladora web y me encantan las hamburguesas</p>
+            </div>
+            <div className="About-location">
+                <p>Lima, Perú</p>
+            </div>
+            <div className="About-social">
+                Social
+            </div>
+        </div>
+    </div>
+);
+
+export default About;
---
+++ src/components/Education.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Education = () => (
+    <div className="Education">
+        <div className="Education-container">
+            <div className="Education-item">
+                <h3>Platzi</h3>
+                <p>Programa de Facebook Developer Circles.</p>
+            </div>
+        </div>
+    </div>
+);
+
+export default Education;
