bab18ba3e095cf722c8e79fea00046bc5fc95d14
Karla Agraz <karla.agraz@gmail.com>
2020-04-17 03:07:21 -0500

    add styled components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import Social from './Social';
+import styled from 'styled-components';
+
+const AboutStyle = styled.div`
+    text-align: center;
+`;
+
+const AboutAvatar = styled.div`
+    padding: 2em 0 0 0;
+`;
+
+const AboutImg = styled.img`
+    border-radius: 100%;
+    width: 160px;
+    height: 160px;
+    border: 2px solid #E91E63;
+    margin: 0 auto;
+    display: block;
+    box-shadow: 0 0 10px rgba(0,0,0,0.6);
+`;
 
 const About = ({ avatar, name, profession, bio, address, social }) => (
-    <div className="About">
+    <AboutStyle>
         <div className="About-container">
-            <div className="About-avatar">
+            <AboutAvatar>
                 <figure>
-                    <img src={avatar} alt={name}/>
+                    <AboutImg src={avatar} alt={name}/>
                 </figure>
-            </div>
+            </AboutAvatar>
             <div className="About-name">
                 <h2>{name}</h2>
             </div>
@@ -23,10 +42,9 @@ const About = ({ avatar, name, profession, bio, address, social }) => (
             </div>
             <div className="About-social">
                 <Social social={social} />
-                social
             </div>
         </div>
-    </div>
+    </AboutStyle>
 );
 
 export default About;
