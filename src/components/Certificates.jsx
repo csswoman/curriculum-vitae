49a2dcb36f665bafd9f033e6e8b6d96962fcdfe7
Karla Agraz <karla.agraz@gmail.com>
2020-04-17 02:53:44 -0500

    add social

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 
 const Certificates = props => (
     <div className="Certificates">
-        <div className="Certificates">
-
-        {props.data.map((cert, index) =>(
+        <div className="Certificates-container">
+            {props.data.map((cert, index) => (
              <div className="Certificates-item" key={`Certificates-${index}`}>
-                <h3>{cert.name}{cert.institution}
-                    <span>{cert.date}}</span>
-                </h3>
+                <h3>{cert.name} @ {cert.institution} <span>{cert.date}}</span></h3>
              <p>{cert.description}</p>
          </div>
         ))}
-
         </div>
     </div>
 );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,18 +2,12 @@ import React from 'react';
 
 const Experience = props => (
     <div className="Experience">
-        <div className="Experience-container">
-
-        {props.data.map((exp, index) =>(
+        {props.data.map((exp, index) => (
              <div className="Experience-item" key={`Exp-${index}`}>
-                <h3>{exp.jobTitle}{exp.company}
-                    <span>{exp.startDate} - {exp.endDate}</span>
-                </h3>
+                <h3>{exp.jobTitle} @ {exp.company} <span>{exp.startDate} - {exp.endDate}</span></h3>
              <p>{exp.jobDescription}</p>
          </div>
         ))}
-
-        </div>
     </div>
 );
 
